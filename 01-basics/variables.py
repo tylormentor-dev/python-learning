ebe37249536e2bbb0f783e1930c9139457213609
tylormentor-dev <tylormentor@gmail.com>
2026-09-17 14:04:10 +0200

    basics of python variables

---
+++ 01-basics/variables.py
@@ -0,0 +1,37 @@
+#Variables are used to store different types of data. 
+#Variables helps you to manipulate data.
+#The word 'Variable' refer to something that can be changed.
+#The naming convention for variables are important because it can be challenging to remember why you used a specific variable when you have to interact with your code months after it was written.
+#When working with other developers it can be difficult to know what a variable refers to unless it has a meaningful label.
+
+#Naming conventions for variables:
+#camelCase: 
+myName
+
+#snake_case:
+my_name
+
+#initializing a variable and assigning a value to it:
+x = 5
+print(x)
+
+#declaring a string:
+x = 'Hello'
+print(x)
+
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+
+
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
+
+a = 10 
+print(a)
+
+del a #deleting a variable
+print(a) #in the terminal, it will state that 'a' is not defined because it has been deleted.
