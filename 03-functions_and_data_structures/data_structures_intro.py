30e8628e09fdc90bcec0751e663ddfc60f4532a6
tylormentor-dev <tylormentor@gmail.com>
2026-09-18 21:11:10 +0200

    list

---
+++ 03-functions_and_data_structures/data_structures_intro.py
@@ -0,0 +1,61 @@
+#A data structure allows you to organize and arrange your data to perform operations on them.
+#Python has the following built-in data structures: List, dictionary, tuple and set.
+#These are all considered non-primitive data structures, meaning they are classed as objects.
+
+#Along with the built-in data structures, Python allows users to create their own.
+#Data structures such as Stacks, Queues and Trees can all be created by the user. 
+
+#Each data structure can be designed to solve a particular problem or optimize a current solution to make it much more performant.
+
+#Mutability and Immutability
+
+#Data Structures can be mutable or immutable.
+# what is mutability? Mutability refers to data inside the data structure that can be modified.
+# you can either change, update, or delete the data when needed. A list is an example of a mutable data structure.
+
+#The opposite of mutable is immutable.
+#An immutable data structure will not allow modification once the data has been set.
+# The tuple is an example of an immutable data structure.
+
+
+# LIST
+#A list is a dynamic array that can hold any datatype.
+#List are always based on an index. 
+#A nested list :
+list1 = [1,[2,3,4], 5, 6 ]
+
+#To print out a list, it can be done in a couple of ways. 
+
+list2 = [1, 2, 3, 4, 5, 6]
+print(*list2) #the star sign 
+
+print(list2, sep = " ") #use the print statement type, put in a seperator  
+
+#Using the insert function
+list1.insert(len(list1, 7))    #it looks for the index of where to insert to. i used the len() function to the length of list1 and put in what the next value should be. 
+print(list1, sep = " ")        #adds 7 to the list.
+
+
+#The append function 
+#Instead of having to specify the index, or where the items should be placed, i can just put in the append keyword. 
+list1.append(7)  
+
+#The extend function
+#adding one or more functions to the list
+list2 = ([7, 8, 9, 10])
+
+#To remove a something a list, we have different options we can use. 
+#using pop:
+
+list1.pop(4) #specify the index or location which item i want to remove. 
+#the fifth number will be removed
+
+#using del 
+del list1[2]
+
+#One of the main reasons we use lists is to iterate through the values and gain access to large amounts of data. 
+#To iterate, i can use a for loop. 
+
+for x in list1:
+    print('Value:', x) #this will print out all the values in the list. 
+
