23a5b20c85d3b9f1431c2bfabec5e451ba6d9003
tylormentor-dev <tylormentor@gmail.com>
2026-09-21 16:27:17 +0200

    file handling

---
+++ 04-errors_exceptions_file_handling/file_handling.py
@@ -0,0 +1,7 @@
+#Python has several built in functions to create and manipulate files. 
+#File Handling includes opening, reading and writing files amongst other operations. 
+
+#There are two file handling functions in python, open and close. 
+#The open function is used for reading, writing and creating files. 
+#The open function accepts two arguments, the first is the file name and/or the file location and the second argument is the mode.
+#The mode indicates what action is required such as reading, writing or creating. 
