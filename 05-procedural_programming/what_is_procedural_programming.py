196b7363d93d7d9201066c242141cd97762fc7b7
tylormentor-dev <tylormentor@gmail.com>
2026-09-24 21:51:47 +0200

    abit of notes

---
+++ 05-procedural_programming/what_is_procedural_programming.py
@@ -0,0 +1,10 @@
+#Python allows for object oriented, procedural and functional programming models or as they're often called paradigms. 
+#Procedural programming is like writing step-by-step instructions a program executes. 
+#The main purpose of a programming module is structure your code. That structure makes it easier to update the code and create new functionality within the code.  
+
+#Procedural programming structures code into procedures sometimes called subroutines or functional sections of code. 
+#The code is made up of logical steps to complete a specific task. 
+
+#DRY : Don't Repeat Yourself. 
+#it's about reducing duplication in code.
+
